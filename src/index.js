d7af6ba266d76afce06c5ac09024ad93fa256d49
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-04-06 20:39:10 +0530

    Final Commit of building react from scratch

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
+// import store from "./store";
+// // import * as actions from "./actionTypes";
+// import * as act from "./actions";
 import store from "./store";
-// import * as actions from "./actionTypes";
-import * as act from "./actions";
+import * as actions from "./actions";
 
 const unsubscribe = store.subscribe(() =>
   console.log("State has been changed", store.getState())
 );
 
-store.dispatch(act.bugAdded("Bug 1 is Added"));
-store.dispatch(act.bugAdded("Bug 2 is Added"));
+store.dispatch(actions.bugAdded("Bug 1 is Added"));
+store.dispatch(actions.bugAdded("Bug 2 is Added"));
 
 // unsubscribe();
 
-// store.dispatch(act.bugRemoved(2));
-store.dispatch(act.bugResolved(2));
+// store.dispatch(actions.bugRemoved(2));
+store.dispatch(actions.bugResolved(2));
 
-store.dispatch(act.bugAdded("Bug 3 is Added"));
+store.dispatch(actions.bugAdded("Bug 3 is Added"));
 
 console.log(store.getState());
+
+// store.dispatch(actions.bugAdded("bug 1"));
