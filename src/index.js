05ca87ea7c16e3421e39b32961ddd4e87aab5413
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-04-10 16:39:17 +0530

    Created Actions and Used Slice

--- src/index.js
+++ src/index.js
@@ -3,27 +3,39 @@
 // import * as act from "./actions";
 // import store from "./store/configStore";
 import configStore from "./store/configStore";
-import * as actions from "./store/bugs";
+// import * as actions from "./store/bugs";
+import * as actions from "./store/projects";
 
 const store = configStore();
 const unsubscribe = store.subscribe(() =>
   console.log("State has been changed", store.getState())
 );
 
-store.dispatch(actions.bugAdded({ description: "Bug 1 is Added" }));
-store.dispatch(actions.bugAdded({ description: "Bug 2 is Added" }));
+////From Actions
+store.dispatch(actions.projectAdded({ name: "Project 1 is Added" }));
+store.dispatch(actions.projectAdded({ name: "Project 2 is Added" }));
+store.dispatch(actions.projectAdded({ name: "Project 3 is Added" }));
+store.dispatch(actions.projectAdded({ name: "Project 4 is Added" }));
 
-// unsubscribe();
+store.dispatch(actions.projectDeleted({ id: 2 }));
 
-store.dispatch(actions.bugResolved({ id: 2 }));
+store.dispatch(actions.projectAdded({ name: "Project 5 is Added" }));
 
-store.dispatch(actions.bugAdded({ description: "Bug 3 is Added" }));
+//// From Bugs
+// store.dispatch(actions.bugAdded({ description: "Bug 1 is Added" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 2 is Added" }));
 
-store.dispatch(actions.bugRemoved({ id: 3 }));
+// // unsubscribe();
 
-store.dispatch(actions.bugAdded({ description: "Bug 4 is Added" }));
+// store.dispatch(actions.bugResolved({ id: 2 }));
 
-store.dispatch(actions.bugUpdated({ id: 4 }));
+// store.dispatch(actions.bugAdded({ description: "Bug 3 is Added" }));
+
+// store.dispatch(actions.bugAdded({ description: "Bug 4 is Added" }));
+
+// store.dispatch(actions.bugRemoved({ id: 3 }));
+
+// store.dispatch(actions.bugUpdated({ id: 4 }));
 
 console.log(store.getState());
 
