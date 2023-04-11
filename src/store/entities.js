c7b70a121daae4f9cb501d1fe7f1cf190c847143
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-04-11 17:07:41 +0530

    Updated Entities

--- src/store/entities.js
+++ src/store/entities.js
@@ -2,11 +2,9 @@ import { combineReducers } from "redux";
 import bugsReducer from "./bugs";
 import projectsReduser from "./projects";
 import usersReducer from "./users";
-import errorsReducer from "./error";
 
 export default combineReducers({
   bugs: bugsReducer,
   projects: projectsReduser,
   users: usersReducer,
-  errors: errorsReducer,
 });
