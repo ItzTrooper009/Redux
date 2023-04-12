636e7f54c836347f28e2ef4b4aad1c9c2fef541e
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-04-12 13:17:15 +0530

    Consuming APIs Initial Commit

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -3,6 +3,7 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducers";
 import logger from "./middleware/logger";
 import toast from "./middleware/toast";
+import api from "./middleware/api";
 
 export default function configStore() {
   return configureStore({
@@ -11,6 +12,7 @@ export default function configStore() {
       ...getDefaultMiddleware(),
       logger({ destination: "console" }),
       toast,
+      api,
     ],
   });
 }
